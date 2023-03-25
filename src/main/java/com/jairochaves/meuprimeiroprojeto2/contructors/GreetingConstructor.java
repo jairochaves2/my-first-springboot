bb7ad45dadc09566a7e219f69be559ec42970902
jairo chaves <jairochaves2@gmail.com>
2023-03-25 10:10:29 -0300

    fix: corrige decorator utilizado

--- src/main/java/com/jairochaves/meuprimeiroprojeto2/contructors/GreetingConstructor.java
+++ src/main/java/com/jairochaves/meuprimeiroprojeto2/contructors/GreetingConstructor.java
@@ -1,7 +1,7 @@
 package com.jairochaves.meuprimeiroprojeto2.contructors;
 
 import com.jairochaves.meuprimeiroprojeto2.models.Greeting;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +11,7 @@ import java.util.concurrent.atomic.AtomicLong;
 public class GreetingConstructor {
     private AtomicLong counter = new AtomicLong();
     String template = "Hello %s!";
-    @GetMapping(path = "/greeting")
+    @RequestMapping(path = "/greeting")
     public Greeting greeting (@RequestParam(value = "name", defaultValue = "world") String name){
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
