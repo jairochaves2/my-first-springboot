437821d73a4f80ba3d0989d576c49b2d7e4882c7
jairo chaves <jairochaves2@gmail.com>
2023-03-25 10:29:00 -0300

    fix: implementa estrutura básica do metodo sum

--- src/main/java/com/jairochaves/meuprimeiroprojeto2/controller/MyMathController.java
+++ src/main/java/com/jairochaves/meuprimeiroprojeto2/controller/MyMathController.java
@@ -1,18 +1,15 @@
 package com.jairochaves.meuprimeiroprojeto2.controller;
 
 import com.jairochaves.meuprimeiroprojeto2.models.MyMath;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class MyMathController {
-    private AtomicLong counter = new AtomicLong();
-    String template = "Hello %s!";
-    @RequestMapping(path = "/greeting")
-    public MyMath greeting (@RequestParam(value = "name", defaultValue = "world") String name){
-        return new MyMath(counter.incrementAndGet(), String.format(template, name));
+    @RequestMapping(value = "sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double sum(@PathVariable(value = "numberOne") String numberOne,
+                      @PathVariable(value = "numberTwo") String numberTwo) {
+        return 1D;
     }
 }
