5cac09ab9ea60e1882867096b979b32c0da46f15
jairo chaves <jairochaves2@gmail.com>
2023-03-25 09:51:57 -0300

    initial projetc

---
+++ src/main/java/com/jairochaves/meuprimeiroprojeto2/contructors/GreetingConstructor.java
@@ -0,0 +1,18 @@
+package com.jairochaves.meuprimeiroprojeto2.contructors;
+
+import com.jairochaves.meuprimeiroprojeto2.models.Greeting;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingConstructor {
+    private AtomicLong counter = new AtomicLong();
+    String template = "Hello %s!";
+    @GetMapping(path = "/greeting")
+    public Greeting greeting (@RequestParam(value = "name", defaultValue = "world") String name){
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+}
